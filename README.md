e14bcc4db117845c44636492e56136fd86282f95
junseopsan <junseopsan@gmail.com>
2023-01-06 15:30:05 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # custom-element-project
+
+```
+<custom-element color="red"> HELLO </custom-element>
+```
+
+```
+CustomElements의 생명주기
+위 예제를 보셨다면 connectedCallback()이라는 콜백 메소드가 있는 것을 볼 수 있습니다.
+
+이는 생명주기(life-cycle) 콜백입니다. 우리가 커스텀 태그를 사용하고, 삭제하고, 옮길 때마다 이 생명주기 콜백이 자동 실행됩니다.
+
+connectedCallback() : 위에서 보았던 메소드로, 문서에 해당 태그 사용이 감지되었고, 문서와 연결을 시도했을 때 호출됩니다.
+
+disconnectedCallback() : 눈치가 빠르시다면 대번에 아실 것입니다. 반대로 요소가 문서에서 연결이 해제 되었을 때, 즉 삭제되었을 때 호출되는 콜백입니다.
+
+adoptedCallback() : 잘 쓸일 없는 콜백일지도 모릅니다. frame과 같은 다른 문서로 커스텀 요소가 이동되었을 때 호출되는 콜백 메소드입니다.
+
+attributeChangedCallback(AttributeName, OldValue, NewValue) : 
+해당 태그에서 등록된 속성 변경을 감지합니다. 감시할 속성을 등록하기 위해서는 
+static get observedAttributes(){ return [‘attr1’,‘attr2’] }
+과 같이 등록해야 합니다.
+
+```
---
+++ main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function() {
+class CustomElement extends HTMLElement {
+    constructor() {
+      // 클래스 초기화. 속성이나 하위 노드는 접근할 수는 없다.
+      super();
+      // Create a shadow root
+      const shadow = this.attachShadow({mode: 'open'});
+      // Create a Div Element
+    //   const color = this.color;
+
+      const div = document.createElement('div');
+      const style = document.createElement("style");
+      shadow.appendChild(div).innerHTML ="Hi, Eelement!";
+      shadow.appendChild(style).innerHTML = `div{ background-color: red; }`;
+    }
+  
+    static get observedAttributes() {
+      // 모니터링 할 속성 이름
+      return ["color"];
+    }
+  
+    connectedCallback() {
+      // DOM에 추가되었다. 렌더링 등의 처리를 하자.
+      this.setAttribute('color', 'blue')
+    //   this.setColor('red')
+    }
+  
+    disconnectedCallback() {
+      // DOM에서 제거되었다. 엘리먼트를 정리하는 일을 하자.
+    }
+  
+    attributeChangedCallback(attrName, oldVal, newVal) {
+      // 속성이 추가/제거/변경되었다.
+      console.log(newVal)
+      console.log(attrName)
+      this[attrName] = newVal;
+    }
+  
+    adoptedCallback(oldDoc, newDoc) {
+      // 다른 Document에서 옮겨져 왔음
+      // 자주 쓸 일은 없을 것.
+    }
+
+    // gathering data from element attributes
+    get color() {
+        return this.getAttribute('color') || '';
+    }
+    // get setColor() {
+    //     return this.setAttribute('color') || '';
+    // }
+  }
+  // <current-time> 태그가 CurrentTime 클래스를 사용하도록 한다.
+  customElements.define("custom-element", CustomElement);
+})();
